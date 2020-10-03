e30265439365b9cc720c3540f3a18a028fb08efe
Tim Broddin <Tim.BRODDIN@VRT.BE>
2020-10-03 11:13:43 +0200

    add adsense

--- pages/_document.js
+++ pages/_document.js
@@ -14,6 +14,11 @@ class MyDocument extends Document {
           <Main />
           <NextScript />
         </body>
+        <script
+          data-ad-client="ca-pub-4111628699519217"
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
+        ></script>
       </Html>
     );
   }
